45d0145ee2d97b540b0cd0f9377f53a162547956
Marco <MarcoAndrade.1980@gmail.com>
2016-07-10 21:10:30 -0400

    New dev.

--- MyTaskList/Default.aspx
+++ MyTaskList/Default.aspx
@@ -1,42 +1,103 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MyTaskList._Default" %>
 
+
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
     <div class="jumbotron">
-        <h1>ASP.NET</h1>
-        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+        <h1>My Task List</h1>
+        <div>
+
+            
+            <asp:Panel runat="server" ID="AddPanel" DefaultButton="AddNewTaskButton">
+                <asp:TextBox runat="server" ID="TaskName" MaxLength="255"></asp:TextBox>
+                <asp:DropDownList runat="server" ID="ColorList" CssClass="LST">
+                    <asp:ListItem Text="White" Value="White"></asp:ListItem>
+                    <asp:ListItem Text="Gray" Value="LightGray"></asp:ListItem>
+                    <asp:ListItem Text="Beige" Value="Beige"></asp:ListItem>
+                    <asp:ListItem Text="Pink" Value="Pink"></asp:ListItem>
+                    <asp:ListItem Text="Green" Value="LightGreen"></asp:ListItem>
+                    <asp:ListItem Text="Blue" Value="LightBlue"></asp:ListItem>
+                </asp:DropDownList>
+                <asp:LinkButton runat="server" ID="AddNewTaskButton" Text="Add New Task" 
+                    OnClick="AddNewTaskButton_Click"></asp:LinkButton>
+            </asp:Panel>
+
+            <asp:Label runat="server" ID="StatusLabel"></asp:Label>
+            <br />
+
+            <asp:RadioButton runat="server" ID="ActiveOnly" CssClass="RadioButtons" Text="Active Items" GroupName="StatusSearch" Checked="true" AutoPostBack="true" OnCheckedChanged="ActiveOnly_CheckedChanged" />&nbsp;
+            <asp:RadioButton runat="server" ID="CompletedOnly" CssClass="RadioButtons" Text="Completed Items" GroupName="StatusSearch" AutoPostBack="true" OnCheckedChanged="CompletedOnly_CheckedChanged" />&nbsp;
+            <asp:RadioButton runat="server" ID="BothStatus" CssClass="RadioButtons" Text="Both" GroupName="StatusSearch" AutoPostBack="true" OnCheckedChanged="BothStatus_CheckedChanged" />
+
+            <asp:GridView runat="server" ID="TasksGrid" ShowHeader="false" DataKeyNames="Id" 
+                AutoGenerateColumns="false" 
+                CssClass="drag_drop_grid GridSrc"  
+                OnRowDataBound="TasksGrid_RowDataBound">
+                <Columns>
+                    <asp:BoundField DataField="Id" ItemStyle-CssClass="IdColumn" />
+                    
+                    <asp:TemplateField ItemStyle-CssClass="ColumnCenter" ItemStyle-Width="30">
+
+                        <ItemTemplate>
+                            <asp:hiddenfield runat="server" id="TaskIdHiddenField" Value='<%# Eval("Id") %>' />
+                            <asp:CheckBox runat="server" ID="Completed" 
+                                Checked='<%#Eval("Status").ToString() == "Completed" ? true : false%>' 
+                                Autopostback="true" OnCheckedChanged="Completed_CheckedChanged" />
+                        </ItemTemplate>
+
+                    </asp:TemplateField>
+
+                    <asp:BoundField DataField="Name" ItemStyle-CssClass="ColumnLeft" 
+                        ItemStyle-Width="420" />
+
+                    <asp:TemplateField ItemStyle-CssClass="ColumnCenter" ItemStyle-Width="50">
+                        <ItemTemplate>
+                            <asp:hiddenfield runat="server" id="TaskIdDelete" Value='<%# Eval("Id") %>' />
+                            <asp:ImageButton runat="server" ID="DeleteTask" 
+                                ImageUrl="~/Images/Delete-Icon.png" 
+                                OnClick="DeleteTask_Click" CssClass="GridButton" 
+                                OnClientClick="if (!confirm('Are you sure you want delete?')) return false;" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                </Columns>
+            </asp:GridView>
+            
+            
+        </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
-        </div>
-    </div>
+<script type="text/javascript">
+    $(function () {
+        $(".drag_drop_grid").sortable({
+            items: 'tr',
+            cursor: 'crosshair',
+            axis: 'y',
+            dropOnEmpty: true,
+            update: function (event, ui) {
+
+                var data = '';
+                var cols = $('.IdColumn');
+                for (i = 0; i < cols.length; i++) {
+                    data = data + '|' + cols[i].innerHTML;
+                }
+                
+                // POST to server using $.post or $.ajax
+                $.ajax({
+                    data: data,
+                    type: 'POST',
+                    url: '/default.aspx?action=sortItems&data=' + data
+                });
+
+                // Update status label
+                $("span[id$='StatusLabel']").text('Task items sorted successfully!');
+                $("span[id$='StatusLabel']").css("color","green");
+
+
+            }
+        });
+
+    });
+</script>
 
 </asp:Content>
--- MyTaskList/Default.aspx.cs
+++ MyTaskList/Default.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Microsoft.AspNet.Identity;
+
 namespace MyTaskList
 {
     public partial class _Default : Page
@@ -12,6 +17,255 @@ namespace MyTaskList
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            // Check authentication
+            if (!this.User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("/Account/Login");
+                return;
+            }
+            
+
+            if (!Page.IsPostBack)
+            {
+
+                if (!string.IsNullOrEmpty(Request.QueryString["action"]) && (Request.QueryString["action"].ToString() == "sortItems"))
+                {
+                    if (!string.IsNullOrEmpty(Request.QueryString["data"])) {
+                        // Resort items
+                        OrderTasks(Request.QueryString["data"]);
+                    }
+
+                }
+
+                LoadTasks();
+
+            }
+
+
+
         }
+
+        protected void AddNewTaskButton_Click(object sender, EventArgs e)
+        {
+            // Add the task
+            AddNewTask();
+
+            // Re-bind the grid
+            LoadTasks();
+
+            // Update status label
+            StatusLabel.ForeColor = System.Drawing.Color.Green;
+            StatusLabel.Text = "Task added successfully!";
+        }
+
+        protected void DeleteTask_Click(object sender, ImageClickEventArgs e)
+        {
+            // Delete task item
+            DeleteTask((ImageButton)sender);
+
+            // Re-bind the grid
+            LoadTasks();
+
+            // Update status label
+            StatusLabel.ForeColor = System.Drawing.Color.Green;
+            StatusLabel.Text = "Task deleted successfully!";
+
+        }
+
+
+
+        private void LoadTasks()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("dbo.GetTasks", conn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            SqlParameter p1 = cmd.CreateParameter();
+            p1.ParameterName = "UserId";
+            p1.DbType = DbType.String;
+            p1.Value = this.User.Identity.GetUserId();
+            cmd.Parameters.Add(p1);
+
+            SqlParameter p2 = cmd.CreateParameter();
+            p2.ParameterName = "Active";
+            p2.DbType = DbType.Boolean;
+            p2.Value = (ActiveOnly.Checked || BothStatus.Checked);
+            cmd.Parameters.Add(p2);
+
+            SqlParameter p3 = cmd.CreateParameter();
+            p3.ParameterName = "Completed";
+            p3.DbType = DbType.Boolean;
+            p3.Value = (CompletedOnly.Checked || BothStatus.Checked);
+            cmd.Parameters.Add(p3);
+
+            adp.Fill(ds);
+
+            conn.Close();
+            
+            TasksGrid.DataSource = ds;
+            TasksGrid.DataBind();
+
+        }
+
+        private void AddNewTask()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("dbo.AddNewTask", conn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            
+            SqlParameter p1 = cmd.CreateParameter();
+            p1.ParameterName = "Name";
+            p1.DbType = DbType.String;
+            p1.Value = TaskName.Text.Trim();
+            cmd.Parameters.Add(p1);
+
+            SqlParameter p2 = cmd.CreateParameter();
+            p2.ParameterName = "UserId";
+            p2.DbType = DbType.String;
+            p2.Value = this.User.Identity.GetUserId();
+            cmd.Parameters.Add(p2);
+
+            SqlParameter p3 = cmd.CreateParameter();
+            p3.ParameterName = "BackColor";
+            p3.DbType = DbType.String;
+            p3.Value = ColorList.SelectedValue;
+            cmd.Parameters.Add(p3);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            TaskName.Text = "";
+        }
+
+        private void OrderTasks(String ids)
+        {
+
+            ids = ids.Trim('|');
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("dbo.OrderTasks", conn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter p1 = cmd.CreateParameter();
+            p1.ParameterName = "data";
+            p1.DbType = DbType.String;
+            p1.Value = ids;
+            cmd.Parameters.Add(p1);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+
+        private void DeleteTask(ImageButton btn)
+        {
+
+            HiddenField TaskIdDelete = (HiddenField)btn.Parent.FindControl("TaskIdDelete");
+
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("dbo.UpdateTaskStatus", conn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            
+            SqlParameter p1 = cmd.CreateParameter();
+            p1.ParameterName = "Id";
+            p1.DbType = DbType.String;
+            p1.Value = TaskIdDelete.Value;
+            cmd.Parameters.Add(p1);
+            
+            SqlParameter p2 = cmd.CreateParameter();
+            p2.ParameterName = "Status";
+            p2.DbType = DbType.String;
+            p2.Value = "Deleted";
+            cmd.Parameters.Add(p2);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+
+        protected void Completed_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox CompletedCheckBox = (CheckBox)sender;
+            HiddenField TaskIdHiddenField = (HiddenField)CompletedCheckBox.Parent.FindControl("TaskIdHiddenField");
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("dbo.UpdateTaskStatus", conn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter p1 = cmd.CreateParameter();
+            p1.ParameterName = "Id";
+            p1.DbType = DbType.String;
+            p1.Value = TaskIdHiddenField.Value;
+            cmd.Parameters.Add(p1);
+
+            SqlParameter p2 = cmd.CreateParameter();
+            p2.ParameterName = "Status";
+            p2.DbType = DbType.String;
+            p2.Value = (CompletedCheckBox.Checked ? "Completed" : "Active");
+            cmd.Parameters.Add(p2);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            // Reload the tasks
+            LoadTasks();
+
+            // Update status label
+            StatusLabel.ForeColor = System.Drawing.Color.Green;
+            StatusLabel.Text = "Task updated successfully!";
+
+        }
+
+        protected void TasksGrid_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow) { 
+                e.Row.BackColor = System.Drawing.Color.FromName(((DataRowView)e.Row.DataItem)["BackColor"].ToString());
+            }
+        }
+
+        protected void ActiveOnly_CheckedChanged(object sender, EventArgs e)
+        {
+            // Reload the tasks
+            LoadTasks();
+
+            // Update status label
+            StatusLabel.Text = "";
+        }
+
+        protected void CompletedOnly_CheckedChanged(object sender, EventArgs e)
+        {
+            // Reload the tasks
+            LoadTasks();
+
+            // Update status label
+            StatusLabel.Text = "";
+        }
+
+        protected void BothStatus_CheckedChanged(object sender, EventArgs e)
+        {
+            // Reload the tasks
+            LoadTasks();
+
+            // Update status label
+            StatusLabel.Text = "";
+        }
+
     }
 }
